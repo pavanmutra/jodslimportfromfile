c8269074404751fbad9823e8427397f3a2d2433a
pavan reddy <pavankumarmutra@gmail.com>
2020-08-14 11:16:41 +0530

    Create artifacts.groovy

---
+++ artifacts.groovy
@@ -0,0 +1,1 @@
+def artifacts=['auditing','client','common-utils','employee-newhire','employee-management','encryption-util','exception-core','integration-management','logging-core','logging-web','notification-management','oauth-authentication','portal-dashboard','portal-ui-framework','reference-mgmt','user-management','reports-management'].toArray()
