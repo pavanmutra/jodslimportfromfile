23472faec09d2d04ddf00b5ead1635a4636c85c6
pavan reddy <pavankumarmutra@gmail.com>
2020-08-14 11:31:44 +0530

    Update artifacts.groovy

--- artifacts.groovy
+++ artifacts.groovy
@@ -1,1 +1,3 @@
-def artifacts=['auditing','client','common-utils','employee-newhire','employee-management','encryption-util','exception-core','integration-management','logging-core','logging-web','notification-management','oauth-authentication','portal-dashboard','portal-ui-framework','reference-mgmt','user-management','reports-management'].toArray()
+def artifacts(){
+  return ['auditing','client','common-utils','employee-newhire','employee-management','encryption-util','exception-core','integration-management','logging-core','logging-web','notification-management','oauth-authentication','portal-dashboard','portal-ui-framework','reference-mgmt','user-management','reports-management'].toArray()
+}
