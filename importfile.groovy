c5c55a0c28ec5c44d78e85e687ad68fe744471ae
pavan reddy <pavankumarmutra@gmail.com>
2020-08-14 11:36:24 +0530

    Update importfile.groovy

--- importfile.groovy
+++ importfile.groovy
@@ -1,6 +1,6 @@
 evaluate(new File("${WORKSPACE}/artifacts.groovy"))
 GroovyShell shell = new GroovyShell()
-def tools = shell.parse(new File('artifacts.groovy'))
+def tools = shell.parse(new File('${WORKSPACE}/artifacts.groovy'))
 artifacts = tools.artifacts()
 
 artifacts.each {
