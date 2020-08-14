fcec385974b495df9c7e25f68fca36539d895ad5
pavan reddy <pavankumarmutra@gmail.com>
2020-08-14 11:38:24 +0530

    Update importfile.groovy

--- importfile.groovy
+++ importfile.groovy
@@ -1,6 +1,6 @@
 evaluate(new File("${WORKSPACE}/artifacts.groovy"))
 GroovyShell shell = new GroovyShell()
-def tools = shell.parse(new File('${WORKSPACE}/artifacts.groovy'))
+def tools = shell.parse(new File('/Users/pmutra/.jenkins/workspace/jobdslimportfromfile/artifacts.groovy'))
 artifacts = tools.artifacts()
 
 artifacts.each {
